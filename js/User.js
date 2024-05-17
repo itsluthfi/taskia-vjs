423ff75e5e4020b4880373b10b730b8577c5fa87
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-05-17 21:48:04 +0700

    feat: create class User and `saveUser` function

---
+++ js/User.js
@@ -0,0 +1,23 @@
+// class User
+
+class User {
+  constructor() {
+    this.users = this.getUsers() || [];
+  }
+
+  saveUser(userData) {
+    const newUser = {
+      id: Date.now(),
+      ...userData,
+    };
+
+    this.users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(this.users));
+
+    return (window.location.href = "../signin.html");
+  }
+
+  getUsers() {
+    return JSON.parse(localStorage.getItem("users")) || [];
+  }
+}
---
+++ js/addUser.js
@@ -0,0 +1,16 @@
+// middleware from HTML to class User
+
+document.addEventListener("DOMContentLoaded", () => {
+  const userForm = document.getElementById("userForm");
+  const userManager = new User();
+
+  userForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const userData = {
+      username: document.getElementById("username").value,
+    };
+
+    userManager.saveUser(userData);
+  });
+});
